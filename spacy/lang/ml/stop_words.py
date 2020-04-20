3f3b4d9ee71f8ab1defd5e3a2336d855a494b295
Sabique <sabique@qburst.com>
2020-04-20 19:57:33 +0530

    Initialize Malayalam Language class

---
+++ spacy/lang/ml/stop_words.py
@@ -0,0 +1,20 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+STOP_WORDS = set(
+
+    """
+അത്
+ഇത്
+ആയിരുന്നു
+ഇല്ല
+അല്ല
+ഉണ്ട്
+നടന്നു
+വന്നു
+പോയി
+ഇരുന്നു
+കുടിച്ചു
+""".split()
+)
